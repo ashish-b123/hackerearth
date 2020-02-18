d9beb083e2cfb2b54ca9190772873b79f1e6549f
ashish-b123 <51282506+ashish-b123@users.noreply.github.com>
2020-02-18 12:33:16 +0530

    Update algo_min_max.py

--- algo_min_max.py
+++ algo_min_max.py
@@ -1,12 +1,3 @@
-it=int(input())
+n=int(input())
 l=list(map(int,input().split()))
-a=[]
-for i in range(it):
-    sum=0
-    for j in range(it):
-        sum=sum+l[j]
-    sum=sum-l[i]
-    a.append(sum)
-mn=min(a)
-mx=max(a)
-print(str(mn)+" "+str(mx))
+print(sum(l)-max(l),sum(l)-min(l))
